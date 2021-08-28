7d741dd417da7355e1961ac5ee8db8046a81f4eb
Grupo8GrupoG <82290835+Grupo8GrupoG@users.noreply.github.com>
2021-08-28 19:51:30 -0300

    Add files via upload

---
+++ Pratica02/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Guilherme e Gabriel!',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ), //Text.
+    ), //Center.
+  );
+}
