59327669ad1bf6e237d1dda009623c1360f9bf6f
Grupo8GrupoG <82290835+Grupo8GrupoG@users.noreply.github.com>
2021-08-28 19:57:18 -0300

    Add files via upload

---
+++ Pratica03/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  String nome = "Guilherme/Gabriel";
+  var dia = "24/08";
+  runApp(
+    Center(
+      child: RichText(
+        textDirection: TextDirection.ltr,
+        text: TextSpan(
+          text: "Olá, ",
+          style: TextStyle(
+            color: Colors.yellow,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            backgroundColor: Colors.black,
+          ), //TextStyle.
+          children: <TextSpan>[
+            TextSpan(
+              text: '$nome, dia da semana $dia',
+              style: TextStyle(
+                color: Colors.white,
+                decoration: TextDecoration.underline,
+                decorationColor: Colors.red,
+                decorationStyle: TextDecorationStyle.double,
+              ),
+            ),
+            TextSpan(
+              text: '! \nBom dia!',
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
